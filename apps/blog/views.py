eccaaf90df3150f557f2bdbf1928adc9175f2842
Regomal <maroeg07@gmail.com>
2022-12-09 15:30:01 +0100

    Tag articles 2

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -19,7 +19,7 @@ def article_view(request, category_id, article_id):
     return render(request, 'blog/article_view.html', {'category': category, 'article': article})
 
 
-def tag_article(request, category_id, article_id, tag_id):
+def tag_article(request, tag_id):
     tag = Tag.objects.get(id=tag_id)
-    articles = Article.objects.filter(tags__name__icontains=tag.name) # <-- Самое адекватное, что я смог найти и понять
+    articles = Article.objects.filter(tags__name__icontains=tag.name) #tags=tag
     return render(request, 'blog/tag_article.html', {'articles': articles, 'tag': tag})
