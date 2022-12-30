b647947f5d14a9b51a063db9a02aeb0c7091b52e
Regomal <maroeg07@gmail.com>
2022-12-30 01:17:05 +0100

    products_1

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from apps.catalog.models import Category
+from apps.catalog.models import Category, Product
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ['name']}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'image_tag_thumbnail', 'price', 'quantity']
+    list_display_links = ['id', 'name']
     prepopulated_fields = {'slug': ['name']}
