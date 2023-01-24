8f348bb7c26523af81f2ea23f98caeb6015d6598
Regomal <117455386+Regomal@users.noreply.github.com>
2023-01-24 17:09:15 +0100

    Order

--- apps/order/models.py
+++ apps/order/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 from apps.catalog.models import Product
 from apps.user.models import User
@@ -12,3 +13,20 @@ class Cart(models.Model):
     class Meta:
         verbose_name = "Корзина"
         verbose_name_plural = "Корзины"
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, verbose_name="Покупатель", on_delete=models.CASCADE)
+    total = models.DecimalField(verbose_name="Итого", max_digits=12, decimal_places=2)
+    first_name = models.CharField(verbose_name="Имя", max_length=255)
+    last_name = models.CharField(verbose_name="Фамилия", max_length=255)
+    email = models.EmailField(verbose_name="E-mail")
+    phone = PhoneNumberField(verbose_name='Телефон')
+    address = models.TextField(verbose_name="Адрес")
+    comment = models.TextField(verbose_name="Коментарий", null=True, blank=True)
+    created_at = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Дата редактирования", auto_now=True)
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
