54fb3ea644075030445bd6e8333f6ba5f137fd58
Regomal <117455386+Regomal@users.noreply.github.com>
2023-02-23 19:42:27 +0100

    info pages 1

--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -14,6 +14,7 @@ class CategoryIndexView(generic.ListView):
         context['breadcrumbs'] = self.breadcrumbs
         return context
 
+
 class ProductsByCategoryView(generic.ListView):
 
     template_name = 'catalog/category.html'
@@ -46,6 +47,7 @@ class ProductsByCategoryView(generic.ListView):
         context['categories'] = self.categories
         context['breadcrumbs'] = self.set_breadcrumbs()
         return context
+
 
 class ProductDetailView(generic.DetailView):
     model = Product
--- apps/main/admin.py
+++ apps/main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from apps.main.models import Page
 
-# Register your models here.
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    pass
+
+
+
