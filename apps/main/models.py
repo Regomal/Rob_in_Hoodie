183d80e1a7ab08579d348a742373d12761da32f3
Regomal <117455386+Regomal@users.noreply.github.com>
2023-02-25 12:08:33 +0100

    home page

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from tinymce.models import HTMLField
+
+from apps.catalog.models import Product
 from apps.main.mixins import MetaTagMixin
 
 
@@ -14,3 +16,18 @@ class Page(MetaTagMixin):
 
     def __str__(self):
         return self.name
+
+
+class ProductSet(models.Model):
+    products = models.ManyToManyField(Product, verbose_name="Товары")
+    name = models.CharField(verbose_name="Название", max_length=255)
+    sort = models.PositiveIntegerField(verbose_name="Сортировка", default=0)
+    is_active = models.BooleanField(verbose_name="Активировано", default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['sort']
+        verbose_name = "Карусель товара"
+        verbose_name_plural = "Карусель товаров"
